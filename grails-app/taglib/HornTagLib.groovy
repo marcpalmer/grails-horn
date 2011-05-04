8643ad9b81f66ca4fa4452fdf5cd8ccdbea46baa
wangjammer7 <wj7@wangjammers.org>
2011-05-04 22:36:20 +0100

    - corrected bug in use of request properties not being prefixed - static def > static - corrected bug whereby nested to hornTag calls were not outputting correctly to out.
    Signed-off-by: wangjammer7 <wj7@wangjammers.org>

--- grails-app/taglib/HornTagLib.groovy
+++ grails-app/taglib/HornTagLib.groovy
@@ -10,18 +10,18 @@ class HornTagLib {
 
     static namespace = 'horn'
 
-    static def KEY_PREFIX_HORNTAGLIB = "com.grailsrocks.horn.hornTagLib"
-    static def KEY_TEMPLATING = HornTagLib.KEY_PREFIX_HORNTAGLIB + ".templating"
+    static KEY_PREFIX_HORNTAGLIB = "com.grailsrocks.horn.hornTagLib"
+    static KEY_TEMPLATING = HornTagLib.KEY_PREFIX_HORNTAGLIB + ".templating"
 
-    static def PATTERN_PPN_ARRAY_INDICES = /(\[([0-9]+)\])/
-    static def PATTERN_PPN_DEREFERENCE = /\./
+    static PATTERN_PPN_ARRAY_INDICES = /(\[([0-9]+)\])/
+    static PATTERN_PPN_DEREFERENCE = /\./
 
-    static def PATTERN_PP_ARRAY_INDICES = /(-([0-9]+))/
-    static def PATTERN_PP_DEREFERENCE = /-/
+    static PATTERN_PP_ARRAY_INDICES = /(-([0-9]+))/
+    static PATTERN_PP_DEREFERENCE = /-/
 
     protected depth = 0
 
-    static def removeAttribute( attributes, attributeName,
+    static removeAttribute( attributes, attributeName,
         defaultValue = "" ) {
         def attributeValue = attributes.remove( attributeName)
 
@@ -30,7 +30,7 @@ class HornTagLib {
     }
 
 
-    static def addAttributeValue( attributeValue, attrs = []) {
+    static addAttributeValue( attributeValue, attrs = []) {
         if ( attributeValue && !attrs.contains( attributeValue.trim()) ) {
             attrs << attributeValue.toString().trim()
         }
@@ -38,7 +38,7 @@ class HornTagLib {
         attrs
     }
 
-    static def addAttributeValues( existingAttributeValue, attrs = [] ) {
+    static addAttributeValues( existingAttributeValue, attrs = [] ) {
         existingAttributeValue.split( " ").each { token ->
             addAttributeValue( token.trim(), attrs)
         }
@@ -68,7 +68,7 @@ class HornTagLib {
         }
     }
 
-    static def encodeCSS( path ) {
+    static encodeCSS( path ) {
         path = path.replaceAll(
             HornTagLib.PATTERN_PPN_ARRAY_INDICES, "-\$2").replaceAll(
             HornTagLib.PATTERN_PPN_DEREFERENCE, "-")
@@ -78,7 +78,7 @@ class HornTagLib {
         !path.startsWith( "_") ?  "_$path" : path
     }
 
-    static def decodeCSS( path ) {
+    static decodeCSS( path ) {
         path = path.replaceFirst( "_", "-").replaceAll(
             HornTagLib.PATTERN_PP_ARRAY_INDICES, "[\$2]").replaceAll(
             HornTagLib.PATTERN_PP_DEREFERENCE, ".")
@@ -86,7 +86,7 @@ class HornTagLib {
         path.startsWith( ".") ? path.substring( ".".length()) : path
     }
 
-    static def isAttributeTruth( attrs, attributeName ) {
+    static isAttributeTruth( attrs, attributeName ) {
         new Boolean( attrs.remove( attributeName))
     }
 
@@ -168,152 +168,152 @@ class HornTagLib {
 
     def a =         { attrs, body ->
                         attrs.tag = "a"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def abbr =      { attrs, body ->
                         def value = safeRemoveAttribute(
                             attrs, 'value', 'abbr')
                         attrs.tag = 'abbr'
                         attrs.title = value
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def b =         { attrs, body ->
                         attrs.tag = "b"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def base =      { attrs, body ->
                         attrs.tag = "base"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def body =      { attrs, body ->
                         attrs.tag = "body"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def br =        { attrs, body ->
                         attrs.tag = "br"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def button =    { attrs, body ->
                         attrs.tag = "button"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def caption =   { attrs, body ->
                         attrs.tag = "caption"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def col =       { attrs, body ->
                         attrs.tag = "col"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def colgroup =  { attrs, body ->
                         attrs.tag = "colgroup"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def div =       { attrs, body ->
                         attrs.tag = "div"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def fieldset =  { attrs, body ->
                         attrs.tag = "fieldset"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def form =      { attrs, body ->
                         attrs.tag = "form"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def head =      { attrs, body ->
                         attrs.tag = "head"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def html =      { attrs, body ->
                         attrs.tag = "html"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def h1 =        { attrs, body ->
                         attrs.tag = "h1"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def h2 =        { attrs, body ->
                         attrs.tag = "h2"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def h3 =        { attrs, body ->
                         attrs.tag = "h3"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def h4 =        { attrs, body ->
                         attrs.tag = "h4"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def h5 =        { attrs, body ->
                         attrs.tag = "h5"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def h6 =        { attrs, body ->
                         attrs.tag = "h6"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def input =     { attrs, body ->
                         attrs.tag = "input"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def label =     { attrs, body ->
                         attrs.tag = "label"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def legend =    { attrs, body ->
                         attrs.tag = "legend"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def li =        { attrs, body ->
                         attrs.tag = "li"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def link =      { attrs, body ->
                         attrs.tag = "link"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def object =    { attrs, body ->
                         attrs.tag = "object"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def ol =        { attrs, body ->
                         attrs.tag = "ol"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def optgroup =  { attrs, body ->
                         attrs.tag = "optgroup"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def option =    { attrs, body ->
                         attrs.tag = "option"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def p =         { attrs, body ->
                         attrs.tag = "p"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def pre =       { attrs, body ->
                         attrs.tag = "pre"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def script =    { attrs, body ->
                         attrs.tag = "script"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def select =    { attrs, body ->
                         attrs.tag = "select"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def span =      { attrs, body ->
                         attrs.tag = "span"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def strong =    { attrs, body ->
                         attrs.tag = "strong"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def style =     { attrs, body ->
                         attrs.tag = "style"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def sub =       { attrs, body ->
                         attrs.tag = "sub"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def sup =       { attrs, body ->
                         attrs.tag = "sup"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def table =     { attrs, body ->
                         attrs.tag = "table"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def tbody =     { attrs, body ->
                         attrs.tag = "tbody"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def textares =  { attrs, body ->
                         attrs.tag = "textares"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def tfoot =     { attrs, body ->
                         attrs.tag = "tfoot"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def thead =     { attrs, body ->
                         attrs.tag = "thead"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def td =        { attrs, body ->
                         attrs.tag = "td"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def th =        { attrs, body ->
                         attrs.tag = "th"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def tr =        { attrs, body ->
                         attrs.tag = "tr"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def tt =        { attrs, body ->
                         attrs.tag = "tt"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
     def ul =        { attrs, body ->
                         attrs.tag = "ul"
-                        hornTag( attrs, body) }
+                        out << hornTag( attrs, body) }
 }
