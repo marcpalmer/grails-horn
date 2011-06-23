960e1b211e26398642b178b920904df1a54beba4
wangjammer7 <wj7@wangjammers.org>
2011-06-23 10:02:40 +0100

    - reinstatement of plugin file
    Signed-off-by: wangjammer7 <wj7@wangjammers.org>

---
+++ HornJqueryGrailsPlugin.groovy
@@ -0,0 +1,11 @@
+class HornJqueryGrailsPlugin {
+    def version = "1.0.29"
+    def grailsVersion = "1.3.1 > *"
+    def dependsOn = [jquery: "1.4 > *", jqueryJson: "2.2.2 > *"]
+    def pluginExcludes = []
+    def author = "Chris Denman"
+    def authorEmail = "chrisdenman@me.com"
+    def title = "Horn"
+    def description = 'Easy HTML data embedding.'
+    def documentation = "http://grails.org/plugin/horn"
+}
