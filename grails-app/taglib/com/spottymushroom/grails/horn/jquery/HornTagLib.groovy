5e57bd44bf2ebc0f0c40cd99ec2f92aabcc9996a
Marc Palmer <marc@anyware.co.uk>
2011-08-03 22:40:53 +0100

    Fixed tabbing

--- grails-app/taglib/com/spottymushroom/grails/horn/jquery/HornTagLib.groovy
+++ grails-app/taglib/com/spottymushroom/grails/horn/jquery/HornTagLib.groovy
@@ -395,153 +395,250 @@ class HornTagLib {
             HornTagLib.removeAttribute( attrs, "value"))
     }
 
-    def a =         { attrs, body ->
-                        attrs.tag = "a"
-                        out << hornTag( attrs, body) }
-    def abbr =      { attrs, body ->
-                        attrs.tag = 'abbr'
-                        attrs.title =
-                            safeRemoveAttribute( attrs, 'value', 'abbr')
-                        out << hornTag( attrs, body) }
-    def b =         { attrs, body ->
-                        attrs.tag = "b"
-                        out << hornTag( attrs, body) }
-    def base =      { attrs, body ->
-                        attrs.tag = "base"
-                        out << hornTag( attrs, body) }
-    def body =      { attrs, body ->
-                        attrs.tag = "body"
-                        out << hornTag( attrs, body) }
-    def br =        { attrs, body ->
-                        attrs.tag = "br"
-                        out << hornTag( attrs, body) }
-    def button =    { attrs, body ->
-                        attrs.tag = "button"
-                        out << hornTag( attrs, body) }
-    def caption =   { attrs, body ->
-                        attrs.tag = "caption"
-                        out << hornTag( attrs, body) }
-    def col =       { attrs, body ->
-                        attrs.tag = "col"
-                        out << hornTag( attrs, body) }
-    def colgroup =  { attrs, body ->
-                        attrs.tag = "colgroup"
-                        out << hornTag( attrs, body) }
-    def div =       { attrs, body ->
-                        attrs.tag = "div"
-                        out << hornTag( attrs, body) }
-    def fieldset =  { attrs, body ->
-                        attrs.tag = "fieldset"
-                        out << hornTag( attrs, body) }
-    def form =      { attrs, body ->
-                        attrs.tag = "form"
-                        out << hornTag( attrs, body) }
-    def head =      { attrs, body ->
-                        attrs.tag = "head"
-                        out << hornTag( attrs, body) }
-    def html =      { attrs, body ->
-                        attrs.tag = "html"
-                        out << hornTag( attrs, body) }
-    def h1 =        { attrs, body ->
-                        attrs.tag = "h1"
-                        out << hornTag( attrs, body) }
-    def h2 =        { attrs, body ->
-                        attrs.tag = "h2"
-                        out << hornTag( attrs, body) }
-    def h3 =        { attrs, body ->
-                        attrs.tag = "h3"
-                        out << hornTag( attrs, body) }
-    def h4 =        { attrs, body ->
-                        attrs.tag = "h4"
-                        out << hornTag( attrs, body) }
-    def h5 =        { attrs, body ->
-                        attrs.tag = "h5"
-                        out << hornTag( attrs, body) }
-    def h6 =        { attrs, body ->
-                        attrs.tag = "h6"
-                        out << hornTag( attrs, body) }
-    def input =     { attrs, body ->
-                        attrs.tag = "input"
-                        out << hornTag( attrs, body) }
-    def label =     { attrs, body ->
-                        attrs.tag = "label"
-                        out << hornTag( attrs, body) }
-    def legend =    { attrs, body ->
-                        attrs.tag = "legend"
-                        out << hornTag( attrs, body) }
-    def li =        { attrs, body ->
-                        attrs.tag = "li"
-                        out << hornTag( attrs, body) }
-    def link =      { attrs, body ->
-                        attrs.tag = "link"
-                        out << hornTag( attrs, body) }
-    def object =    { attrs, body ->
-                        attrs.tag = "object"
-                        out << hornTag( attrs, body) }
-    def ol =        { attrs, body ->
-                        attrs.tag = "ol"
-                        out << hornTag( attrs, body) }
-    def optgroup =  { attrs, body ->
-                        attrs.tag = "optgroup"
-                        out << hornTag( attrs, body) }
-    def option =    { attrs, body ->
-                        attrs.tag = "option"
-                        out << hornTag( attrs, body) }
-    def p =         { attrs, body ->
-                        attrs.tag = "p"
-                        out << hornTag( attrs, body) }
-    def pre =       { attrs, body ->
-                        attrs.tag = "pre"
-                        out << hornTag( attrs, body) }
-    def script =    { attrs, body ->
-                        attrs.tag = "script"
-                        out << hornTag( attrs, body) }
-    def select =    { attrs, body ->
-                        attrs.tag = "select"
-                        out << hornTag( attrs, body) }
-    def span =      { attrs, body ->
-                        attrs.tag = "span"
-                        out << hornTag( attrs, body) }
-    def strong =    { attrs, body ->
-                        attrs.tag = "strong"
-                        out << hornTag( attrs, body) }
-    def style =     { attrs, body ->
-                        attrs.tag = "style"
-                        out << hornTag( attrs, body) }
-    def sub =       { attrs, body ->
-                        attrs.tag = "sub"
-                        out << hornTag( attrs, body) }
-    def sup =       { attrs, body ->
-                        attrs.tag = "sup"
-                        out << hornTag( attrs, body) }
-    def table =     { attrs, body ->
-                        attrs.tag = "table"
-                        out << hornTag( attrs, body) }
-    def tbody =     { attrs, body ->
-                        attrs.tag = "tbody"
-                        out << hornTag( attrs, body) }
-    def textarea =  { attrs, body ->
-                        attrs.tag = "textarea"
-                        out << hornTag( attrs, body) }
-    def tfoot =     { attrs, body ->
-                        attrs.tag = "tfoot"
-                        out << hornTag( attrs, body) }
-    def thead =     { attrs, body ->
-                        attrs.tag = "thead"
-                        out << hornTag( attrs, body) }
-    def td =        { attrs, body ->
-                        attrs.tag = "td"
-                        out << hornTag( attrs, body) }
-    def th =        { attrs, body ->
-                        attrs.tag = "th"
-                        out << hornTag( attrs, body) }
-    def tr =        { attrs, body ->
-                        attrs.tag = "tr"
-                        out << hornTag( attrs, body) }
-    def tt =        { attrs, body ->
-                        attrs.tag = "tt"
-                        out << hornTag( attrs, body) }
-    def ul =        { attrs, body ->
-                        attrs.tag = "ul"
-                        out << hornTag( attrs, body) }
+    def a = { attrs, body ->
+        attrs.tag = "a"
+        out << hornTag( attrs, body) 
+    }
+    
+    def abbr = { attrs, body ->
+        attrs.tag = 'abbr'
+        attrs.title =
+            safeRemoveAttribute( attrs, 'value', 'abbr')
+        out << hornTag( attrs, body)
+    }
+    
+    def b = { attrs, body ->
+        attrs.tag = "b"
+        out << hornTag( attrs, body)
+    }
+    
+    def base = { attrs, body ->
+        attrs.tag = "base"
+        out << hornTag( attrs, body)
+    }
+    
+    def body = { attrs, body ->
+        attrs.tag = "body"
+        out << hornTag( attrs, body)
+    }
+    
+    def br = { attrs, body ->
+        attrs.tag = "br"
+        out << hornTag( attrs, body)
+    }
+    
+    def button = { attrs, body ->
+        attrs.tag = "button"
+        out << hornTag( attrs, body)
+    }
+    
+    def caption = { attrs, body ->
+        attrs.tag = "caption"
+        out << hornTag( attrs, body)
+    }
+    
+    def col = { attrs, body ->
+        attrs.tag = "col"
+        out << hornTag( attrs, body)
+    }
+    
+    def colgroup = { attrs, body ->
+        attrs.tag = "colgroup"
+        out << hornTag( attrs, body)
+    }
+    
+    def div = { attrs, body ->
+        attrs.tag = "div"
+        out << hornTag( attrs, body)
+    }
+    
+    def fieldset = { attrs, body ->
+        attrs.tag = "fieldset"
+        out << hornTag( attrs, body)
+    }
+    
+    def form = { attrs, body ->
+        attrs.tag = "form"
+        out << hornTag( attrs, body)
+    }
+    
+    def head = { attrs, body ->
+        attrs.tag = "head"
+        out << hornTag( attrs, body)
+    }
+    
+    def html = { attrs, body ->
+        attrs.tag = "html"
+        out << hornTag( attrs, body)
+    }
+    
+    def h1 = { attrs, body ->
+        attrs.tag = "h1"
+        out << hornTag( attrs, body)
+    }
+    
+    def h2 = { attrs, body ->
+        attrs.tag = "h2"
+        out << hornTag( attrs, body)
+    }
+    
+    def h3 = { attrs, body ->
+        attrs.tag = "h3"
+        out << hornTag( attrs, body)
+    }
+    
+    def h4 = { attrs, body ->
+        attrs.tag = "h4"
+        out << hornTag( attrs, body)
+    }
+    
+    def h5 = { attrs, body ->
+        attrs.tag = "h5"
+        out << hornTag( attrs, body)
+    }
+    
+    def h6 = { attrs, body ->
+        attrs.tag = "h6"
+        out << hornTag( attrs, body)
+    }
+    
+    def input = { attrs, body ->
+        attrs.tag = "input"
+        out << hornTag( attrs, body)
+    }
+    
+    def label = { attrs, body ->
+        attrs.tag = "label"
+        out << hornTag( attrs, body)
+    }
+    
+    def legend = { attrs, body ->
+        attrs.tag = "legend"
+        out << hornTag( attrs, body)
+    }
+    
+    def li = { attrs, body ->
+        attrs.tag = "li"
+        out << hornTag( attrs, body)
+    }
+    
+    def link = { attrs, body ->
+        attrs.tag = "link"
+        out << hornTag( attrs, body)
+    }
+    
+    def object = { attrs, body ->
+        attrs.tag = "object"
+        out << hornTag( attrs, body)
+    }
+    
+    def ol = { attrs, body ->
+        attrs.tag = "ol"
+        out << hornTag( attrs, body)
+    }
+    
+    def optgroup = { attrs, body ->
+        attrs.tag = "optgroup"
+        out << hornTag( attrs, body)
+    }
+    
+    def option = { attrs, body ->
+        attrs.tag = "option"
+        out << hornTag( attrs, body)
+    }
+    
+    def p = { attrs, body ->
+        attrs.tag = "p"
+        out << hornTag( attrs, body)
+    }
+    
+    def pre = { attrs, body ->
+        attrs.tag = "pre"
+        out << hornTag( attrs, body)
+    }
+    
+    def script = { attrs, body ->
+        attrs.tag = "script"
+        out << hornTag( attrs, body)
+    }
+    
+    def select = { attrs, body ->
+        attrs.tag = "select"
+        out << hornTag( attrs, body)
+    }
+    
+    def span = { attrs, body ->
+        attrs.tag = "span"
+        out << hornTag( attrs, body)
+    }
+    
+    def strong = { attrs, body ->
+        attrs.tag = "strong"
+        out << hornTag( attrs, body)
+    }
+    
+    def style = { attrs, body ->
+        attrs.tag = "style"
+        out << hornTag( attrs, body)
+    }
+    
+    def sub = { attrs, body ->
+        attrs.tag = "sub"
+        out << hornTag( attrs, body)
+    }
+    
+    def sup = { attrs, body ->
+        attrs.tag = "sup"
+        out << hornTag( attrs, body)
+    }
+    
+    def table = { attrs, body ->
+        attrs.tag = "table"
+        out << hornTag( attrs, body)
+    }
+    
+    def tbody = { attrs, body ->
+        attrs.tag = "tbody"
+        out << hornTag( attrs, body)
+    }
+    
+    def textarea = { attrs, body ->
+        attrs.tag = "textarea"
+        out << hornTag( attrs, body)
+    }
+    
+    def tfoot = { attrs, body ->
+        attrs.tag = "tfoot"
+        out << hornTag( attrs, body)
+    }
+    
+    def thead = { attrs, body ->
+        attrs.tag = "thead"
+        out << hornTag( attrs, body)
+    }
+    
+    def td = { attrs, body ->
+        attrs.tag = "td"
+        out << hornTag( attrs, body)
+    }
+    
+    def th = { attrs, body ->
+        attrs.tag = "th"
+        out << hornTag( attrs, body)
+    }
+    
+    def tr = { attrs, body ->
+        attrs.tag = "tr"
+        out << hornTag( attrs, body)
+    }
+    
+    def tt = { attrs, body ->
+        attrs.tag = "tt"
+        out << hornTag( attrs, body)
+    }
+    
+    def ul = { attrs, body ->
+        attrs.tag = "ul"
+        out << hornTag( attrs, body)
+    }
 }
